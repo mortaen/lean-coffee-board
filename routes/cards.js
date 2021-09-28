d8bade0777ea95829c5b0e90ad67ee5af89a6c3c
Morten Kolbe <kolbe.mor@gmail.com>
2021-09-28 17:20:29 +0200

    implement get handlers and corresponding tests

--- routes/cards.js
+++ routes/cards.js
@@ -17,8 +17,17 @@ let cards = [
 ]
 
 router.get('/', (req, res) => {
-  res.set('Content-type', 'text/html; charset=utf-8')
-  res.send(req.body)
+  res.status(200).json(cards)
+})
+
+router.get('/:id', (req, res) => {
+  const exampleCard = cards.find(card => card.id === req.params.id)
+  if (exampleCard) {
+    res.status(200).json(exampleCard)
+  } else {
+    const error = { message: 'Could not find object with that id.' }
+    res.status(404).json(error)
+  }
 })
 
 router.post('/', (req, res) => {
