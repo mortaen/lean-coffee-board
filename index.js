f1f5dd0a4c959ba8ecea916f7b77c1ac1255dbc8
Morten Kolbe <kolbe.mor@gmail.com>
2021-09-28 11:18:33 +0200

    Add routing to index.js & move handlers to card.js

--- index.js
+++ index.js
@@ -4,27 +4,7 @@ const port = 3000
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.set('Content-type', 'text/html; charset=utf-8')
-  res.send('Hello World!')
-})
-
-app.post('/api/cards', (req, res) => {
-  console.log(req.body)
-  res.send('This was a POST request')
-})
-
-app.put('/api/cards', (req, res) => {
-  res.send('This was a PUT request')
-})
-
-app.patch('/api/cards', (req, res) => {
-  res.send('This was a PATCH request')
-})
-
-app.delete('/api/cards', (req, res) => {
-  res.send('This was a DELETE request')
-})
+app.use('/api/cards', require('./routes/cards'))
 
 app.listen(port, () => {
   {
---
+++ routes/cards.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.set('Content-type', 'text/html; charset=utf-8')
+  res.send(req.body)
+})
+
+router.post('/', (req, res) => {
+  res.send(req.body.text)
+})
+
+router.put('/:id', (req, res) => {
+  console.log(req.body)
+  res.send(req.body)
+})
+
+router.patch('/:id', (req, res) => {
+  console.log(req.body)
+  res.send(req.body)
+})
+
+router.delete('/:id', (req, res) => {
+  res.send(req.body)
+})
+
+module.exports = router
