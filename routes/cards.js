a037179014fc4a158b2f49aeec921c867385261e
Morten Kolbe <kolbe.mor@gmail.com>
2021-09-29 15:39:01 +0200

    delete unused code

--- routes/cards.js
+++ routes/cards.js
@@ -44,11 +44,6 @@ router.post('/', (req, res) => {
   })
     .then(data => res.status(200).json(data))
     .catch(error => res.status(404).json(error))
-
-  // const newCard = { text, author, id: nanoid() }
-
-  // cards = [...cards, newCard]
-  // res.status(200).json(newCard)
 })
 
 router.put('/:id', (req, res) => {
